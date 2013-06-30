eaad42fae294fff62e87a4a06c0c3779a85ec4e5
takezoe <takezoe@gmail.com>
2013-07-01 03:29:33 +0900

    Trying to parse Solr result using SolrJ's XMLResponseParser.

--- src/main/scala/jp/sf/amateras/solr/scala/async/AsyncSolrServer.scala
+++ src/main/scala/jp/sf/amateras/solr/scala/async/AsyncSolrServer.scala
@@ -4,6 +4,8 @@ import org.apache.solr.client.solrj.SolrQuery
 import com.ning.http.client.AsyncHttpClient
 import com.ning.http.client.AsyncCompletionHandler
 import com.ning.http.client.Response
+import org.apache.solr.client.solrj.impl.XMLResponseParser
+import org.apache.solr.client.solrj.response.QueryResponse
 
 class AsyncSolrServer {
   
@@ -14,10 +16,28 @@ class AsyncSolrServer {
     
     new AsyncHttpClient().prepareGet("http://localhost:8983/solr/select?" + solrQuery.getQuery)
       .execute(new AsyncCompletionHandler[T](){
-        override def onCompleted(response: Response ): T = {
+        override def onCompleted(response: Response): T = {
+          val parser = new XMLResponseParser
+          val namedList = parser.processResponse(response.getResponseBodyAsStream, "UTF-8")
+          val queryResponse = new QueryResponse
+          queryResponse.setResponse(namedList)
+          
+          println(queryResponse.getResults().getNumFound())
+          
           callback()
         } 
       })
   }
   
+}
+
+object AsyncTest extends App {
+  val query = new SolrQuery()
+  query.setQuery("*:*")
+  
+  val server = new AsyncSolrServer()
+  server.query(query){() =>
+    println("OK!!")
+  }
+ 
 }
