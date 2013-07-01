ba4cb12afabfb0d3ec056174d2e17f3399cc2198
takezoe <takezoe@gmail.com>
2013-07-01 17:18:06 +0900

    Add an example of async API.

---
+++ src/main/scala/jp/sf/amateras/solr/scala/sample/AsyncSolrClientSample.scala
@@ -0,0 +1,22 @@
+package jp.sf.amateras.solr.scala.sample
+
+import jp.sf.amateras.solr.scala.async._
+import jp.sf.amateras.solr.scala.Order
+
+object AsyncSolrClientSample extends App {
+
+  val client = new AsyncSolrClient("http://localhost:8983/solr")
+
+  client.query("name:%name%")
+        .fields("id", "manu", "name")
+        .facetFields("manu")
+        .sortBy("id", Order.asc)
+        .getResultAsMap(Map("name" -> "ThinkPad -X201s")){ result =>
+    result.documents.foreach { doc =>
+      println("id: " + doc("id"))
+      println("  manu: " + doc.get("manu").getOrElse("<NULL>"))
+      println("  name: " + doc("name"))
+    }
+  }
+
+}
