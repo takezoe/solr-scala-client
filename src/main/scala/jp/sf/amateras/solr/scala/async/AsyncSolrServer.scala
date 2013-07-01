3fcea74559a212dc5d1d9c2210fea24c54195a37
takezoe <takezoe@gmail.com>
2013-07-01 17:17:45 +0900

    Encode query string.

--- src/main/scala/jp/sf/amateras/solr/scala/async/AsyncSolrServer.scala
+++ src/main/scala/jp/sf/amateras/solr/scala/async/AsyncSolrServer.scala
@@ -4,14 +4,14 @@ import org.apache.solr.client.solrj.SolrQuery
 import org.apache.solr.client.solrj.impl.XMLResponseParser
 import org.apache.solr.client.solrj.response.QueryResponse
 
-import com.ning.http.client.AsyncCompletionHandler
-import com.ning.http.client.AsyncHttpClient
-import com.ning.http.client.Response
+import com.ning.http.client._
+
+import java.net.URLEncoder
 
 class AsyncSolrServer(url: String, httpClient: AsyncHttpClient = new AsyncHttpClient()) {
   
   def query(solrQuery: SolrQuery)(callback: (QueryResponse) => Unit) = {
-    httpClient.prepareGet(url + "/select?q=" + solrQuery.getQuery + "&wt=xml")
+    httpClient.prepareGet(url + "/select?q=" + URLEncoder.encode(solrQuery.getQuery, "UTF-8") + "&wt=xml")
       .execute(new AsyncCompletionHandler[Unit](){
         override def onCompleted(response: Response): Unit = {
           val parser = new XMLResponseParser
