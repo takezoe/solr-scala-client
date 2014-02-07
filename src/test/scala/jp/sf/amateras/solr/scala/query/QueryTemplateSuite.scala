17311809cd1c933cfe0f59c1ac53fed34dfa0ff7
takezoe <takezoe@gmail.com>
2014-02-08 02:36:54 +0900

    Fix test case.

--- src/test/scala/jp/sf/amateras/solr/scala/query/QueryTemplateSuite.scala
+++ src/test/scala/jp/sf/amateras/solr/scala/query/QueryTemplateSuite.scala
@@ -6,7 +6,7 @@ class QueryTemplateSuite extends FunSuite {
   test("Test for parameter replacement"){
     implicit val parser = new DefaultExpressionParser()
 	val query = new QueryTemplate("$KEY$: %VALUE%").merge(Map("KEY" -> "title", "VALUE" -> "(Scala in Action)"))
-	assert(query == """title: "\\(Scala in Action\\)"""")
+	assert(query == """title: "\(Scala in Action\)"""")
   }
 
 }
