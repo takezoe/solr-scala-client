c8e27b66d08e5d0c4b0a1c7cf5e352e2ae9ad319
Palak <palak.shah@orbis-vis.com>
2020-02-27 11:48:58 +0530

    facet pivot included in mapquery result

--- src/main/scala/com/github/takezoe/solr/scala/QueryResult.scala
+++ src/main/scala/com/github/takezoe/solr/scala/QueryResult.scala
@@ -1,35 +1,37 @@
 package com.github.takezoe.solr.scala
 
 /**
- * The result of query which is executed by QueryBuilder#getResultAsMap().
- *
- * @param numFound the total number of hits
- * @param documents the list of documents which are matched to the query
- * @param groups the result of the grouped query which were specified by QueryBuilder#groupBy()
- * @param facetFields the facet count of fields which were specified by QueryBuilder#facetFields()
- */
-case class MapQueryResult(
-    numFound: Long,
-    numGroupsFound: Long,
-    documents: List[DocumentMap],
-    groups: Map[String, List[Group]],
-    facetFields: Map[String, Map[String, Long]],
-    qTime:Int)
+  * The result of query which is executed by QueryBuilder#getResultAsMap().
+  *
+  * @param numFound the total number of hits
+  * @param documents the list of documents which are matched to the query
+  * @param groups the result of the grouped query which were specified by QueryBuilder#groupBy()
+  * @param facetFields the facet count of fields which were specified by QueryBuilder#facetFields()
+  * @param facetPivots the facet pivot fields which were specified by QueryBuilder##facetPivots()
+  */
+case class MapQueryResult(numFound: Long,
+                          numGroupsFound: Long,
+                          documents: List[DocumentMap],
+                          groups: Map[String, List[Group]],
+                          facetFields: Map[String, Map[String, Long]],
+                          facetPivots: Map[String, List[Pivot]],
+                          qTime: Int)
 
 /**
- * The result of query which is executed by QueryBuilder#getResultAs().
- *
- * @param numFound the total number of hits
- * @param documents the list of documents which are matched to the query
- * @param facetFields the facet count of fields which were specified by QueryBuilder#facetFields()
- * @tparam T
- */
-case class CaseClassQueryResult[T](
-    numFound: Long,
-    documents: List[T],
-    facetFields: Map[String, Map[String, Long]])
+  * The result of query which is executed by QueryBuilder#getResultAs().
+  *
+  * @param numFound the total number of hits
+  * @param documents the list of documents which are matched to the query
+  * @param facetFields the facet count of fields which were specified by QueryBuilder#facetFields()
+  * @tparam T
+  */
+case class CaseClassQueryResult[T](numFound: Long,
+                                   documents: List[T],
+                                   facetFields: Map[String, Map[String, Long]])
 
-case class Group(
-    value: String,
-    numFound: Long,
-    documents: List[DocumentMap])
+case class Group(value: String, numFound: Long, documents: List[DocumentMap])
+
+case class Pivot(field: String, count: Int, pivot: List[Pivot], value: String)
+
+
+
